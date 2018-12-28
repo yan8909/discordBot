8dcd3ab008c7ff7d4a942b747adca4adca162bc5
yan8909 <43824130+yan8909@users.noreply.github.com>
2018-12-29 06:17:02 +0800

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Commando = require('discord.js-commando');
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const Token = 'NTI4MjUzMjI1NTQ0MTg3OTA0.Dwf3lQ.S_r20GsniAt0HBaW28SQllcImDk';
+const Token = 'process.env.Token';
 
 bot.login(Token);
-id = '520481540027711488';
+var id = '520481540027711488';
 bot.on('message', function(message){
     
-    mention = '<@' + id + '>';
+    var mention = '<@' + id + '>';
     if(message.content == '<:yee:524609574540673065>'){
-        message.channel.send('mention');
+        message.channel.send(mention);
     }
     
 });
