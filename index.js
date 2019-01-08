f8badbb351b7bb6d7a8f71511be024d9dbde335d
yan8909 <43824130+yan8909@users.noreply.github.com>
2019-01-08 22:37:37 +0800

    add commands

--- index.js
+++ index.js
@@ -4,12 +4,68 @@ const bot = new Discord.Client();
 const Token = process.env.Token;
 
 bot.login(Token);
-var id = '520481540027711488';
+
+var Yeeid = '520481540027711488';
+var mention = '<@' + Yeeid + '>';
+
 bot.on('message', function(message){
-    
-    var mention = '<@' + id + '>';
-    if(message.content == '<:yee:524609574540673065>'){
-        message.channel.send(mention);
+
+    switch(message.content)
+    {
+        case '<:yee:524609574540673065>' :
+            message.channel.send(mention);
+            break;
+
+        case "上車" :
+            message.channel.send({files: ['./images/djv.gif']
+            })
+            .catch(console.error);
+            break;
+
+        case "怕" :
+            message.channel.send({files: ['./images/megumin.gif']
+            })
+            .catch(console.error);
+            break;
+        
+        case "大爆射" :
+            message.channel.send({files: ['./images/bigshoot.gif']
+            })
+            .catch(console.error);
+            break;
+
+        case "惠射" :
+            message.channel.send({files: ['./images/megu_shoot.gif']
+            })
+            .catch(console.error);
+            break;
+
+        case "嗨起來" :
+            var num = Math.floor( Math.random() * 25 )
+            message.channel.send({files: [`./images/giri/${num}.gif`]
+            })
+            .catch(console.error);
+            break;
+
+        case "舔" :
+            message.channel.send({files: ['./images/lick.gif']
+            })
+            .catch(console.error);
+            break;
+
+        case "!help" :
+            message.channel.send(
+                "```\n" +
+                "指令表\n" +
+                "- 上車\n" +
+                "- 怕\n" +
+                "- 大爆射\n" +
+                "- 惠射\n" +
+                "- 嗨起來\n"+
+                "- 舔" +
+                "```"
+            );
+            
     }
-    
+
 });
