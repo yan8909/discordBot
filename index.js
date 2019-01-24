16c47895278c44ace928f39007f0282703026d68
yan8909 <43824130+yan8909@users.noreply.github.com>
2019-01-25 02:21:13 +0800

    fix command

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('message', function(message){
             break;
 
         case "上車" :
-            message.channel.send({files: ['./images/djv.gif']
+            message.channel.send('沒時間解釋了 快上車!', {files: ['./images/djv.gif']
             })
             .catch(console.error);
             break;
@@ -29,16 +29,21 @@ bot.on('message', function(message){
             break;
         
         case "大爆射" :
-            message.channel.send({files: ['./images/bigshoot.gif']
+            var img = [
+                './images/bigshoot.gif',
+                './images/megu_shoot.gif',
+                './images/shoot_spin.gif'
+            ]
+            message.channel.send({files: [ img[Math.floor(Math.random() * img.length)] ]
             })
             .catch(console.error);
             break;
 
-        case "惠射" :
-            message.channel.send({files: ['./images/megu_shoot.gif']
-            })
-            .catch(console.error);
-            break;
+        // case "惠射" :
+        //     message.channel.send({files: ['']
+        //     })
+        //     .catch(console.error);
+        //     break;
 
         case "嗨起來" :
             var num = Math.floor( Math.random() * 25 )
@@ -72,20 +77,32 @@ bot.on('message', function(message){
             break;
 
         case "!help" :
-            message.channel.send(
-                "```\n" +
-                "指令表\n" +
-                "-上車\n" +
-                "-怕\n" +
-                "-大爆射\n" +
-                "-惠射\n" +
-                "-嗨起來\n"+
-                "-舔\n" +
-                "-阿福\n" +
-                "-大王\n" +
-                "-gacha" +
-                "```"
-            );
+            var embed = new Discord.RichEmbed()
+                        .setTitle("指令表")
+                        .addField("上車")
+                        .addField("怕")
+                        .addField("大爆射")
+                        .addField("嗨起來")
+                        .addField("舔")
+                        .addField("阿福")
+                        .addField("大王")
+                        .addField("gacha")
+            message.channel.send({embed});
+            break;
+            //     embed:{}
+            //     "```\n" +
+            //     "指令表\n" +
+            //     "-上車\n" +
+            //     "-怕\n" +
+            //     "-大爆射\n" +
+            //     "-惠射\n" +
+            //     "-嗨起來\n"+
+            //     "-舔\n" +
+            //     "-阿福\n" +
+            //     "-大王\n" +
+            //     "-gacha" +
+            //     "```"
+            // );
             
     }
 
