7fcb185e49e754e597b43a0a7f0a54f069700fdc
unknown <yan8909@gmail.com>
2024-06-11 20:42:46 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const { config }  = require("dotenv");
+const bot = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences
+	],
+});
 const fs = require('fs');
-const Token = process.env.Token;
-const translate = require('@vitalets/google-translate-api');
+config();
+// const Token = process.env.Token;
+const Token = process.env.DiscordToken;
 bot.login(Token);
 
-var Yeeid = '520481540027711488';
+var Yeeid = process.env.YeeId;
 var mention = '<@' + Yeeid + '>';
 var yeeArray = ['yee', 'Yee', 'ㄏㄏ', '廠廠']
+var guild = bot.guilds.cache.get(process.env.ServerId);
 
-// bot.on('ready', () => {
+bot.on('ready', () => {
+    var guid = bot.guilds.cache.filter(function(obj){return obj==process.env.ServerId}).first();
+    guid.members.fetch({ withPresences: true });
 //     fs.readdir('./images/shoot', function(err, files){
 //         if(err) {
 //             return console.error(err);
@@ -20,75 +33,72 @@ var yeeArray = ['yee', 'Yee', 'ㄏㄏ', '廠廠']
 //         console.log( file );
 //         console.log(num);
 //         console.log(Math.floor(num));
-        
+//         console.log(bot.guilds.cache)
+//         var guid = bot.guilds.cache.filter(function(obj){return obj=='524539217167122442'}).first();
+//         guid.members.cache.forEach(function(v, i){
+//             console.log(v)
+//         })
+//         console.log(guid.members.cache.filter(m=>m.client.presence.status == "online"));
 //     });
-//   });
+  });
 
-bot.on('message', function(message){
 
-    //翻譯
-    if(message.content.startsWith('tr?')){
-        var msg = message.content;
-        var srcMsg = msg.slice(msg.indexOf(' ') + 1);
-        var toLanguage = msg.slice(3, msg.indexOf(' '));
-        
-        translate(srcMsg, msg.indexOf(' ')>3 ? {to: toLanguage} : {to: 'zh-tw'}).then(res => {
-            message.channel.send({embed: {
-                author: {
-                    name: message.author.username,
-                    icon_url: message.author.avatarURL
-                },
-                description: res.text
-            }})
-            }).catch(err => {
-                console.error(err);
-        });
-    }
-
+bot.on('messageCreate', function(message){
     //狀態
-    if(message.content.startsWith('!status')){
-        var msg = message.content;
-        var userName = msg.slice(msg.indexOf(' ') + 1);
-        var user = bot.users.find('username', userName);
+    // if(message.content.startsWith('!status')){
+    //     var msg = message.content;
+    //     var userName = msg.slice(msg.indexOf(' ') + 1);
+    //     var user = bot.users.find('username', userName);
 
-        if(userName == '<:yee:524609574540673065>' || yeeArray.includes(userName) ){
-            user = bot.users.find('username', 'salmon');
-        }
+    //     if(userName == '<:yee:524609574540673065>' || yeeArray.includes(userName) ){
+    //         user = bot.users.find('username', 'salmon');
+    //     }
 
-        if(user) {
-            if(user.presence.status == 'offline'){
-                message.channel.send(userName + ' (¦3[▓▓]  有事請留言');
-            }else if (user.presence.status == 'idle' || user.presence.status == 'dnd') {
-                message.channel.send(userName + ' 在忙' + '<:Uknow:554584844542410752>');
-            }
-            else{
-                message.channel.send(user.presence.status);
-            }
-        }
-        else{
-            message.channel.send('誰???')
-        }
+    //     if(user) {
+    //         if(user.presence.status == 'offline'){
+    //             message.channel.send(userName + ' (¦3[▓▓]  有事請留言');
+    //         }else if (user.presence.status == 'idle' || user.presence.status == 'dnd') {
+    //             message.channel.send(userName + ' 在忙' + '<:Uknow:554584844542410752>');
+    //         }
+    //         else{
+    //             message.channel.send(user.presence.status);
+    //         }
+    //     }
+    //     else{
+    //         message.channel.send('誰???')
+    //     }
 
 
-    }    
+    // }    
 
     switch(message.content)
     {
         case '<:yee:524609574540673065>' :
-            var user = bot.users.cache.find(user => user.id == Yeeid);
-            console.log(user.presence.status);
-            if(user.presence.status == 'offline'){
-                message.channel.send(mention + ' (¦3[▓▓]  有事請留言');
-            }else if (user.presence.status == 'idle' || user.presence.status == 'dnd') {
-                message.channel.send(mention + ' 在忙' + '<:Uknow:554584844542410752>');
+            var guid = bot.guilds.cache.filter(function(obj){return obj==process.env.ServerId}).first();
+            // guid.members.cache.forEach(function(v, i){
+            //     console.log(v)
+            // })
+            // console.log(guid.members.cache.filter(m=>m.client.presence.status == "online"));
+            var yee = guid.members.cache.filter(m=>m.user.id == Yeeid).first();
+            var yeeStatus = "";
+            if(yee.presence == null){
+                yeeStatus = "offline"
             }
             else{
+                yeeStatus = yee.presence.status
+            } 
+            if(yeeStatus == 'offline'){
+                message.channel.send(mention + ' (¦3[▓▓]  有事請留言');
+            }else if (yeeStatus == 'idle' || yeeStatus == 'dnd') {
+                message.channel.send(mention + ' 在忙' + '<:Uknow:554584844542410752>');
+            }
+            else if (yeeStatus == 'online'){
                 message.channel.send(mention + ' ─=≡Σ((( つ•̀ω•́)つ');
             }
-            break;
+             break;
 
         case "上車" :
-            message.channel.send('沒時間解釋了 快上車!', {files: ['./images/djv.gif']
+            message.channel.send({content:'沒時間解釋了 快上車!', files: ['./images/djv.gif']
             })
             .catch(console.error);
             break;
@@ -121,10 +131,20 @@ bot.on('message', function(message){
             })
             .catch(console.error);
             break;
-
+        case "bro":
+            fs.readdir('./images/bro', function(err, files){
+                if(err) {
+                    console.error;
+                }
+                var bro  = files;
+                message.channel.send({files: [ './images/bro/' + bro[Math.floor(Math.random() * bro.length)] ]
+                })
+                .catch(console.error);
+            });
+            break;
         case "嗨起來" :
             var num = Math.floor( Math.random() * 25 )
-            message.channel.send("ギリギリeye~  ギリギリmind~",{files: [`./images/giri/${num}.gif`]
+            message.channel.send({content:"ギリギリeye~  ギリギリmind~", files: [`./images/giri/${num}.gif`]
             })
             .catch(console.error);
             break;
@@ -165,8 +185,6 @@ bot.on('message', function(message){
                 "-阿福\n" +
                 "-大王\n" +
                 "-gacha\n" +
-                "-tr?目標語言 原文\n" +
-                " 語言清單:https://github.com/matheuss/google-translate-api/blob/master/languages.js\n" +
                 "```"
             );
             break;
