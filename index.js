f24e633d3224a3dca5f690785bb54470435a49d3
unknown <yan8909@gmail.com>
2024-06-16 11:29:03 +0800

    helth check

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const bot = new Client({
 	],
 });
 const fs = require('fs');
+
+// helth check
+const express = require("express");
+const router = express.Router({});
+router.get('/', async (_req, res, _next) => {
+
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        res.status(503).send();
+    }
+});
+// export router with all routes included
+module.exports = router;
+
 config();
 // const Token = process.env.Token;
 const Token = process.env.DiscordToken;
